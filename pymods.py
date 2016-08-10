bfe2056aff735a9e84bdcb834edee2e0e4c223e8
mrmiguez <r.m.miguez@gmail.com>
2016-08-10 14:36:22 -0400

    playing with classes

--- pymods.py
+++ pymods.py
@@ -28,183 +28,185 @@ Elements to add 2016-02-11:
     location - physical location
     recordInfo (??)
 '''
-def mods_note(mods_record, nameSpace_dict):
-    allNotes = []
-    for note in mods_record.iterfind('./{%s}note' % nameSpace_dict['mods']):
-        if len(note.attrib) >= 1:
-            if note.attrib['type'] is not None:
-                #other option: allNotes.append('{0} : {1}'.format(note.attrib['type'], note.text))
-                typed_note = {note.attrib['type'] : note.text}
-                allNotes.append(typed_note)
+class mods:
+
+    def note(record, nameSpace_dict):
+        allNotes = []
+        for note in record.iterfind('./{%s}note' % nameSpace_dict['mods']):
+            if len(note.attrib) >= 1:
+                if note.attrib['type'] is not None:
+                    #other option: allNotes.append('{0} : {1}'.format(note.attrib['type'], note.text))
+                    typed_note = {note.attrib['type'] : note.text}
+                    allNotes.append(typed_note)
+            else:
+                allNotes.append(note.text)
+        return allNotes
+
+
+    def digitalOrigin(record, nameSpace_dict):
+        if record.find('.//{%s}digitalOrigin' % nameSpace_dict['mods']) is not None:
+            return record.find('.//{%s}digitalOrigin' % nameSpace_dict['mods']).text
         else:
-            allNotes.append(note.text)
-    return allNotes
+            return None
 
 
-def mods_digitalOrigin(mods_record, nameSpace_dict):
-    if mods_record.find('.//{%s}digitalOrigin' % nameSpace_dict['mods']) is not None:
-        return mods_record.find('.//{%s}digitalOrigin' % nameSpace_dict['mods']).text
-    else:
-        return None
-
-
-def mods_language(mods_record, nameSpace_dict):
-    allLanguages = []
-    if mods_record.find('.//{%s}language' % nameSpace_dict['mods']) is not None:
-        for language in mods_record.iterfind('.//{%s}language' % nameSpace_dict['mods']):
-            languages = {}
-            for term in language.iterchildren():
-                languages[term.attrib['type']] = term.text
-            allLanguages.append(languages)
-        return allLanguages
-    else:
-        return None
-
-
-def mods_genre_text(mods_record, nameSpace_dict):
-    if mods_record.find('.//{%s}genre' % nameSpace_dict['mods']) is not None:
-        genre = mods_record.find('.//{%s}genre' % nameSpace_dict['mods'])
-        return genre.text
-    else:
-        return None
-
-def mods_genre_URIs(mods_record, nameSpace_dict):
-    genreURIs = []
-    for genre_elem in mods_record.iterfind('.//{%s}genre' % nameSpace_dict['mods']):
-        if len(genre_elem.attrib) >= 1:
-            genreURI = {}
-            for key in genre_elem.attrib.keys():
-                genreURI[key] = genre_elem.attrib[key]
-            genreURIs.append(genreURI)
-    return genreURIs
-  
-
-def mods_typeOfResource(mods_record, nameSpace_dict):
-    if mods_record.find('.//{%s}typeOfResource' % nameSpace_dict['mods']) is not None:
-        typeOfResource = mods_record.find('.//{%s}typeOfResource' % nameSpace_dict['mods'])
-        return typeOfResource.text
-    else:
-        return None
-
-
-def nameGen(names, fullName):
-    keys = []
-    for key in names.keys():
-        keys.append(key)
-    if all(x in keys for x in ['family', 'given', 'termsOfAddress', 'date']):
-        fullName = fullName + names['family'] + ', ' + names['given'] + ', ' + names['termsOfAddress'] + ' ' + names[
-            'date']
-    elif all(x in keys for x in ['family', 'given', 'date']):
-        fullName = fullName + names['family'] + ', ' + names['given'] + ' ' + names['date']
-    elif all(x in keys for x in ['family', 'given', 'termsOfAddress']):
-        fullName = fullName + names['family'] + ', ' + names['given'] + ', ' + names['termsOfAddress']
-    elif all(x in keys for x in ['family', 'termsOfAddress', 'date']):
-        fullName = fullName + names['family'] + ', ' + names['termsOfAddress'] + ' ' + names['date']
-    elif all(x in keys for x in ['given', 'termsOfAddress', 'date']):
-        fullName = fullName + names['given'] + ', ' + names['termsOfAddress'] + ' ' + names['date']
-    elif all(x in keys for x in ['family', 'given']):
-        fullName = fullName + names['family'] + ', ' + names['given']
-    elif all(x in keys for x in ['family', 'date']):
-        fullName = fullName + names['family'] + ', ' + names['date']
-    elif all(x in keys for x in ['family', 'termsOfAddress']):
-        fullName = fullName + names['family'] + ', ' + names['termsOfAddress']
-    elif all(x in keys for x in ['given', 'date']):
-        fullName = fullName + names['given'] + ', ' + names['date']
-    elif all(x in keys for x in ['given', 'termsOfAddress']):
-        fullName = fullName + names['given'] + ', ' + names['termsOfAddress']
-    elif all(x in keys for x in ['termsOfAddress', 'date']):
-        fullName = fullName + ', ' + names['termsOfAddress'] + ' ' + names['date']
-    elif 'date' in keys:
-        fullName = fullName + ', ' + names['date']
-    elif 'termsOfAddress' in keys:
-        fullName = fullName + ', ' + names['termsOfAddress']
-    return fullName
-
-
-def mods_name_generator(mods_record, nameSpace_dict):
-    allNames = []
-    for name in mods_record.iterfind('./{%s}name' % nameSpace_dict['mods']):
-        fullName = ""
-        if len(name.findall('./{%s}namePart' % nameSpace_dict['mods'])) > 1:
-            # Multipart name
-            names = {}
-            for namePart in name.findall('./{%s}namePart' % nameSpace_dict['mods']):
-                if 'type' not in namePart.attrib.keys():
-                    fullName = namePart.text
-                elif 'type' in namePart.attrib.keys():
-                    names[namePart.attrib['type']] = namePart.text
-            fullName = nameGen(names, fullName)
+    def language(record, nameSpace_dict):
+        allLanguages = []
+        if record.find('.//{%s}language' % nameSpace_dict['mods']) is not None:
+            for language in record.iterfind('.//{%s}language' % nameSpace_dict['mods']):
+                languages = {}
+                for term in language.iterchildren():
+                    languages[term.attrib['type']] = term.text
+                allLanguages.append(languages)
+            return allLanguages
         else:
-            # Single part name
-            fullName = fullName + name.find('./{%s}namePart' % nameSpace_dict['mods']).text
-        allNames.append(fullName)
-        return allNames
+            return None
 
 
-def mods_title_generator(mods_record, nameSpace_dict):
-    allTitles = []
-    for title in mods_record.iterfind('.//{%s}titleInfo' % nameSpace_dict['mods']):
-        if title.find('./{%s}nonSort' % nameSpace_dict['mods']) is not None and title.find(
-                        './{%s}title' % nameSpace_dict['mods']) is not None and title.find(
-                        './{%s}subTitle' % nameSpace_dict['mods']) is not None:
-            titleFull = title.find('./{%s}nonSort' % nameSpace_dict['mods']).text + ' ' + title.find(
-                './{%s}title' % nameSpace_dict['mods']).text + ': ' + title.find(
-                './{%s}subTitle' % nameSpace_dict['mods']).text
-        elif title.find('./{%s}nonSort' % nameSpace_dict['mods']) is not None and title.find(
-                        './{%s}title' % nameSpace_dict['mods']) is not None:
-            titleFull = title.find('./{%s}nonSort' % nameSpace_dict['mods']).text + ' ' + title.find(
-                './{%s}title' % nameSpace_dict['mods']).text
+    def genre_text(record, nameSpace_dict):
+        if record.find('.//{%s}genre' % nameSpace_dict['mods']) is not None:
+            genre = record.find('.//{%s}genre' % nameSpace_dict['mods'])
+            return genre.text
         else:
-            titleFull = title.find('./{%s}title' % nameSpace_dict['mods']).text
-        allTitles.append(titleFull)
-        return allTitles
+            return None
+
+    def genre_URIs(record, nameSpace_dict):
+        genreURIs = []
+        for genre_elem in record.iterfind('.//{%s}genre' % nameSpace_dict['mods']):
+            if len(genre_elem.attrib) >= 1:
+                genreURI = {}
+                for key in genre_elem.attrib.keys():
+                    genreURI[key] = genre_elem.attrib[key]
+                genreURIs.append(genreURI)
+        return genreURIs
+      
+
+    def typeOfResource(record, nameSpace_dict):
+        if record.find('.//{%s}typeOfResource' % nameSpace_dict['mods']) is not None:
+            typeOfResource = record.find('.//{%s}typeOfResource' % nameSpace_dict['mods'])
+            return typeOfResource.text
+        else:
+            return None
 
 
-def mods_date_generator(mods_record, nameSpace_dict):
-    if mods_record.find('./{%s}originInfo/{%s}copyrightDate' % (
-    nameSpace_dict['mods'], nameSpace_dict['mods'])) is not None:
-        date = mods_record.find(
-            './{%s}originInfo/{%s}copyrightDate' % (nameSpace_dict['mods'], nameSpace_dict['mods'])).text
-    elif mods_record.find(
-                    './{%s}originInfo/{%s}dateCreated' % (nameSpace_dict['mods'], nameSpace_dict['mods'])) is not None:
-        date = mods_record.find(
-            './{%s}originInfo/{%s}dateCreated' % (nameSpace_dict['mods'], nameSpace_dict['mods'])).text
-    elif mods_record.find(
-                    './{%s}originInfo/{%s}dateIssued' % (nameSpace_dict['mods'], nameSpace_dict['mods'])) is not None:
-        date = mods_record.find(
-            './{%s}originInfo/{%s}dateIssued' % (nameSpace_dict['mods'], nameSpace_dict['mods'])).text
-    elif mods_record.find(
-                    './{%s}originInfo/{%s}dateOther' % (nameSpace_dict['mods'], nameSpace_dict['mods'])) is not None:
-        date = mods_record.find(
-            './{%s}originInfo/{%s}dateOther' % (nameSpace_dict['mods'], nameSpace_dict['mods'])).text
-    else:
-        date = "No date"
-    return date
+    def nameGen(names, fullName):
+        keys = []
+        for key in names.keys():
+            keys.append(key)
+        if all(x in keys for x in ['family', 'given', 'termsOfAddress', 'date']):
+            fullName = fullName + names['family'] + ', ' + names['given'] + ', ' + names['termsOfAddress'] + ' ' + names[
+                'date']
+        elif all(x in keys for x in ['family', 'given', 'date']):
+            fullName = fullName + names['family'] + ', ' + names['given'] + ' ' + names['date']
+        elif all(x in keys for x in ['family', 'given', 'termsOfAddress']):
+            fullName = fullName + names['family'] + ', ' + names['given'] + ', ' + names['termsOfAddress']
+        elif all(x in keys for x in ['family', 'termsOfAddress', 'date']):
+            fullName = fullName + names['family'] + ', ' + names['termsOfAddress'] + ' ' + names['date']
+        elif all(x in keys for x in ['given', 'termsOfAddress', 'date']):
+            fullName = fullName + names['given'] + ', ' + names['termsOfAddress'] + ' ' + names['date']
+        elif all(x in keys for x in ['family', 'given']):
+            fullName = fullName + names['family'] + ', ' + names['given']
+        elif all(x in keys for x in ['family', 'date']):
+            fullName = fullName + names['family'] + ', ' + names['date']
+        elif all(x in keys for x in ['family', 'termsOfAddress']):
+            fullName = fullName + names['family'] + ', ' + names['termsOfAddress']
+        elif all(x in keys for x in ['given', 'date']):
+            fullName = fullName + names['given'] + ', ' + names['date']
+        elif all(x in keys for x in ['given', 'termsOfAddress']):
+            fullName = fullName + names['given'] + ', ' + names['termsOfAddress']
+        elif all(x in keys for x in ['termsOfAddress', 'date']):
+            fullName = fullName + ', ' + names['termsOfAddress'] + ' ' + names['date']
+        elif 'date' in keys:
+            fullName = fullName + ', ' + names['date']
+        elif 'termsOfAddress' in keys:
+            fullName = fullName + ', ' + names['termsOfAddress']
+        return fullName
 
 
-def fsudl_purl_search(mods_record, nameSpace_dict):
-    purl = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)')
-    for url in mods_record.iterfind('./{%s}location/{%s}url' % (nameSpace_dict['mods'], nameSpace_dict['mods'])):
-        match = purl.search(url.text)
-        if match:
-            return match.group()
+    def name_generator(record, nameSpace_dict):
+        allNames = []
+        for name in record.iterfind('./{%s}name' % nameSpace_dict['mods']):
+            fullName = ""
+            if len(name.findall('./{%s}namePart' % nameSpace_dict['mods'])) > 1:
+                # Multipart name
+                names = {}
+                for namePart in name.findall('./{%s}namePart' % nameSpace_dict['mods']):
+                    if 'type' not in namePart.attrib.keys():
+                        fullName = namePart.text
+                    elif 'type' in namePart.attrib.keys():
+                        names[namePart.attrib['type']] = namePart.text
+                fullName = nameGen(names, fullName)
+            else:
+                # Single part name
+                fullName = fullName + name.find('./{%s}namePart' % nameSpace_dict['mods']).text
+            allNames.append(fullName)
+            return allNames
 
 
-def fsudl_pid_search(mods_record, nameSpace_dict):
-    pid = re.compile('fsu:[0-9]*')
-    for identifier in mods_record.iterfind('.//{%s}identifier' % nameSpace_dict['mods']):
-        match = pid.search(identifier.text)
-        if match:
-            return match.group()
+    def title_generator(record, nameSpace_dict):
+        allTitles = []
+        for title in record.iterfind('.//{%s}titleInfo' % nameSpace_dict['mods']):
+            if title.find('./{%s}nonSort' % nameSpace_dict['mods']) is not None and title.find(
+                            './{%s}title' % nameSpace_dict['mods']) is not None and title.find(
+                            './{%s}subTitle' % nameSpace_dict['mods']) is not None:
+                titleFull = title.find('./{%s}nonSort' % nameSpace_dict['mods']).text + ' ' + title.find(
+                    './{%s}title' % nameSpace_dict['mods']).text + ': ' + title.find(
+                    './{%s}subTitle' % nameSpace_dict['mods']).text
+            elif title.find('./{%s}nonSort' % nameSpace_dict['mods']) is not None and title.find(
+                            './{%s}title' % nameSpace_dict['mods']) is not None:
+                titleFull = title.find('./{%s}nonSort' % nameSpace_dict['mods']).text + ' ' + title.find(
+                    './{%s}title' % nameSpace_dict['mods']).text
+            else:
+                titleFull = title.find('./{%s}title' % nameSpace_dict['mods']).text
+            allTitles.append(titleFull)
+            return allTitles
 
 
-def mods_subject_generator(mods_record, nameSpace_dict):
-    allSubjects = []
-    for subject in mods_record.iterfind('.//{%s}subject' % nameSpace_dict['mods']):
-        fullSubject = []
-        for subjectTerm in subject.iterchildren():
-            fullSubject.append({subjectTerm.tag[28: ] : subjectTerm.text})
-#        if fullSubject:
-#            allSubjects.append(fullSubject)
-        allSubjects.append(fullSubject)
-    return allSubjects
+    def date_generator(record, nameSpace_dict):
+        if record.find('./{%s}originInfo/{%s}copyrightDate' % (
+        nameSpace_dict['mods'], nameSpace_dict['mods'])) is not None:
+            date = record.find(
+                './{%s}originInfo/{%s}copyrightDate' % (nameSpace_dict['mods'], nameSpace_dict['mods'])).text
+        elif record.find(
+                        './{%s}originInfo/{%s}dateCreated' % (nameSpace_dict['mods'], nameSpace_dict['mods'])) is not None:
+            date = record.find(
+                './{%s}originInfo/{%s}dateCreated' % (nameSpace_dict['mods'], nameSpace_dict['mods'])).text
+        elif record.find(
+                        './{%s}originInfo/{%s}dateIssued' % (nameSpace_dict['mods'], nameSpace_dict['mods'])) is not None:
+            date = record.find(
+                './{%s}originInfo/{%s}dateIssued' % (nameSpace_dict['mods'], nameSpace_dict['mods'])).text
+        elif record.find(
+                        './{%s}originInfo/{%s}dateOther' % (nameSpace_dict['mods'], nameSpace_dict['mods'])) is not None:
+            date = record.find(
+                './{%s}originInfo/{%s}dateOther' % (nameSpace_dict['mods'], nameSpace_dict['mods'])).text
+        else:
+            date = "No date"
+        return date
+        
+    def subject_generator(record, nameSpace_dict):
+        allSubjects = []
+        for subject in record.iterfind('.//{%s}subject' % nameSpace_dict['mods']):
+            fullSubject = []
+            for subjectTerm in subject.iterchildren():
+                fullSubject.append({subjectTerm.tag[28: ] : subjectTerm.text})
+    #        if fullSubject:
+    #            allSubjects.append(fullSubject)
+            allSubjects.append(fullSubject)
+        return allSubjects
+
+class fsudl:
+    
+    def purl_search(mods_record, nameSpace_dict):
+        purl = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)')
+        for url in mods_record.iterfind('./{%s}location/{%s}url' % (nameSpace_dict['mods'], nameSpace_dict['mods'])):
+            match = purl.search(url.text)
+            if match:
+                return match.group()
+
+
+    def pid_search(mods_record, nameSpace_dict):
+        pid = re.compile('fsu:[0-9]*')
+        for identifier in mods_record.iterfind('.//{%s}identifier' % nameSpace_dict['mods']):
+            match = pid.search(identifier.text)
+            if match:
+                return match.group()
--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 from lxml import etree
-import pymods
+from pymods import mods, fsudl
 
 NS = {'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/', 'dc': 'http://purl.org/dc/elements/1.1/', 'mods': 'http://www.loc.gov/mods/v3', 'dcterms': 'http://purl.org/dc/terms'}
 tree = etree.parse('fsu_cookbooksandherbals.xml')
 root = tree.getroot()
 for record in root.iterfind('.//{%s}mods' % NS['mods']):
 #    if pymods.mods_note(record, NS) is not None:
-    for subject in pymods.mods_subject_generator(record, NS):
-        print(pymods.fsudl_pid_search(record, NS), ':', subject)
+    for subject in mods.subject_generator(record, NS):
+        print(fsudl.pid_search(record, NS), ':', subject)
