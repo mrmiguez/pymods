4aa8c3a771acfedce6140d752f7ce4f9541e4a99
mrmiguez <r.m.miguez@gmail.com>
2016-12-07 10:52:25 -0500

    cleaning up

--- dev/test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-from lxml import etree
-from pymods import mods, fsudl, oai_dc
-
-for record in mods.load('fsu_cookbooksandherbals.xml'):
-#    for iid in fsudl.local_identifier(record):
-    print(fsudl.pid_search(record), ':', fsudl.local_identifier(record))
