965e88d90ffed33f03f43c80b567eddf44238344
mrmiguez <r.m.miguez@gmail.com>
2017-03-08 15:13:41 -0500

    prepping for dist.

--- setup.py
+++ setup.py
@@ -2,13 +2,27 @@ from setuptools import setup, find_packages
 
 setup(
     name="pymods",
-    version="0.0.4",
-    packages=find_packages(),
+    version="0.0.5",
+    packages=find_packages(exclude=['tests*']),
     install_requires=['lxml >= 2.3'],
     author="Matthew Miguez",
     author_email="r.m.miguez@gmail.com",
     description=(
         "Utility class wrapping lxml for serializing data from MODS v3.4 XML metadata."
     ),
+    url='https://github.com/mrmiguez/pymods',
     keywords="MODS metadata xml",
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Text Processing :: Markup :: XML'
+    ]
 )
