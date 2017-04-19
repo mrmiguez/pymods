8db86bc649547841b470b78cee7f2f948a3b6b92
mrmiguez <r.m.miguez@gmail.com>
2017-04-19 11:51:46 -0400

    testing

--- pymods/constants.py
+++ pymods/constants.py
@@ -1,7 +1,12 @@
-NAMESPACES = { None: '{http://www.loc.gov/mods/v3}',
-               'oai_dc': '{http://www.openarchives.org/OAI/2.0/oai_dc/}',
-               'dc': '{http://purl.org/dc/elements/1.1/}',
-               'mods': '{http://www.loc.gov/mods/v3}',
-               'dcterms': '{http://purl.org/dc/terms}',
-               'xlink': '{http://www.w3.org/1999/xlink}',
-               'repox': '{http://repox.ist.utl.pt}' }
+NAMESPACES = {
+              'oai_dc': '{http://www.openarchives.org/OAI/2.0/oai_dc/}',
+              'dc': '{http://purl.org/dc/elements/1.1/}',
+              'mods': '{http://www.loc.gov/mods/v3}',
+              'dcterms': '{http://purl.org/dc/terms}',
+              'xlink': '{http://www.w3.org/1999/xlink}',
+              'repox': '{http://repox.ist.utl.pt}'}
+
+DATE_FIELDS = {'{0}dateIssued'.format(NAMESPACES['mods']),
+               '{0}dateCreated'.format(NAMESPACES['mods']),
+               '{0}copyrightDate'.format(NAMESPACES['mods']),
+               '{0}dateOther'.format(NAMESPACES['mods'])}
