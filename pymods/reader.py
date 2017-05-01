6fe3912b520b274bf745c2e2d72b03def2a642c8
mrmiguez <r.m.miguez@gmail.com>
2017-05-01 15:25:32 -0400

    starting from scratch

--- pymods/reader.py
+++ pymods/reader.py
@@ -1,85 +1,50 @@
 from lxml import etree
-
-from pymods import Record
+from pymods import Record, MODSRecord, OAIRecord
 from pymods.constants import NAMESPACES
-from pymods.exceptions import NameSpaceInvalid
 
 
 class Reader(etree.XMLParser):
-    """
-    A base class for all iterating readers in the pymods package.
-    """
-    def __init__(self, mods_target):
-        """
 
-        """
-        if mods_target is not None:
-            self.mods_target = mods_target
-            self.tree = etree.parse(self.mods_target)
-            self.root = self.tree.getroot()
-        self.record_list = []
+    def __init__(self, file_location, iter_elem, parser=None):
+        super(Reader, self).__init__()
 
-    def close(self):
-        """
+        if parser is not None:
+            self.iterator = etree.parse(file_location, parser=parser).iter(iter_elem)
+        else:
+            self.iterator = etree.parse(file_location).iter(iter_elem)
 
-        :return:
-        """
-        if self.mods_target is not None:
-            self.mods_target.close()
-            self.mods_target = None
+    def __next__(self):
+        return Record(next(self.iterator))
 
     def __iter__(self):
-        return iter(self.record_list)
+        return self
 
-    def __len__(self):
-        return len(self.record_list)
+    def __index__(self):
+        return self
+
+    def __int__(self):
+        return self
 
 
 class MODSReader(Reader):
 
-    def __init__(self, mods_target):
-        """
+    def __init__(self, file_location):
+        mods_parser_registration = etree.ElementDefaultClassLookup(element=MODSRecord)
+        mods_parser = etree.XMLParser()
+        mods_parser.set_element_class_lookup(mods_parser_registration)
+        super(MODSReader, self).__init__(file_location, '{0}mods'.format(NAMESPACES['mods']), parser=mods_parser)
 
-        :param mods_target:
-        """
-        super(MODSReader, self).__init__(mods_target)
-
-        self.mods_parser_registration = etree.ElementDefaultClassLookup(element=Record)
-        self.mods_parser = etree.XMLParser()
-        self.mods_parser.set_element_class_lookup(self.mods_parser_registration)
-
-        self.record_list = []
-        for record in self.root.iter('{0}mods'.format(NAMESPACES['mods'])):
-            record_tree = etree.ElementTree(record, parser=self.mods_parser)
-            record_root = record_tree.getroot()
-            #record_root = Record(record_tree.getroot())
-            self.record = Record(record_root)
-            self.record_list.append(self.record)
+    def __next__(self):
+        return MODSRecord(next(self.iterator))
 
 
 class OAIReader(Reader):
-    def __init__(self, mods_target):
-        """
 
-        :param mods_target:
-        """
-        super(OAIReader, self).__init__()
-        if mods_target is not None:
-            self.mods_target = mods_target
-            self.tree = etree.parse(self.mods_target)
-            self.root = self.tree.getroot()
-
-    def close(self):
-        """
-
-        :return:
-        """
-        if self.mods_target is not None:
-            self.mods_target.close()
-            self.mods_target = None
+    def __init__(self, file_location):
+        oai_parser_registration = etree.ElementDefaultClassLookup(element=OAIRecord)
+        oai_parser = etree.XMLParser()
+        oai_parser.set_element_class_lookup(oai_parser_registration)
+        super(OAIReader, self).__init__(file_location, '{*}record', parser=oai_parser)
 
     def __next__(self):
-        """
-
-        :return:
-        """
+        return OAIRecord(next(self.iterator))
