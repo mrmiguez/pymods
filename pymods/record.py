1f64d2f71e8773ebaf8c182af0e0d87b602e69ab
mrmiguez <r.m.miguez@gmail.com>
2017-05-01 15:55:48 -0400

    starting from scratch

--- pymods/record.py
+++ pymods/record.py
@@ -19,8 +19,15 @@ class OAIRecord(Record):
     def _init(self):
         super(OAIRecord, self)._init()
         if '{http://repox.ist.utl.pt}' in self.tag:
+            #print('check') #debug
             #for elem in self.iter('{http://repox.ist.utl.pt}*'):
             #    print(elem)
-            self.oai_id = self.find('{0}record/{0}header/{0}identifier'.format(NAMESPACES['repox'])).text
+            try:
+                self.oai_id = self.find('{0}record'.format(NAMESPACES['repox'])).attrib['id']
+            except AttributeError:
+                pass
         elif '{http://www.openarchives.org/OAI/2.0/oai_dc/}' in self.tag:
-            self.oai_id = self.attrib['id']
+            try:
+                self.oai_id = self.find('{0}header/{0}identifier'.format(NAMESPACES['oai_dc']))
+            except AttributeError:
+                pass
