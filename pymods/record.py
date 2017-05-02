6202ed2ae434674f189a366d4cee82b2a7661723
mrmiguez <r.m.miguez@gmail.com>
2017-05-02 15:12:18 -0400

    improving

--- pymods/record.py
+++ pymods/record.py
@@ -23,12 +23,16 @@ class OAIRecord(Record):
     def oai_urn(self):
         if '{http://repox.ist.utl.pt}' in self.tag:
             try:
-                oai_id = self.attrib['id']
-                return oai_id
+                return self.attrib['id']
             except AttributeError:
                 pass
         elif '{http://www.openarchives.org/OAI/2.0/oai_dc/}' in self.tag:
             try:
-                return self.find('{0}header/{0}identifier'.format(NAMESPACES['oai_dc']))
+                return self.find('{0}header/{0}identifier'.format(NAMESPACES['oai_dc'])).text
+            except AttributeError:
+                pass
+        else:
+            try:
+                return self.find('{}header/{}identifier').text
             except AttributeError:
                 pass
