ea2679ebe83529ea7529989e63f3b213462eaa4e
mrmiguez <r.m.miguez@gmail.com>
2017-11-07 07:39:57 -0800

    same dumb typo: pymods.constants.NAMESPACES

--- pymods/constants.py
+++ pymods/constants.py
@@ -1,4 +1,4 @@
-NAMESPACES = {'oai_dc': '{http://www.openarchives.org/OAI/2.0/oai_dc/}',
+NAMESPACES = {'oai_dc': '{http://www.openarchives.org/OAI/2.0/}',
               'dc': '{http://purl.org/dc/elements/1.1/}',
               'mods': '{http://www.loc.gov/mods/v3}',
               'dcterms': '{http://purl.org/dc/terms}',
@@ -9,3 +9,7 @@ DATE_FIELDS = ['{0}dateIssued'.format(NAMESPACES['mods']),
                '{0}dateCreated'.format(NAMESPACES['mods']),
                '{0}copyrightDate'.format(NAMESPACES['mods']),
                '{0}dateOther'.format(NAMESPACES['mods'])]
+
+NS_MAP = {'mods': 'http://www.loc.gov/mods/v3',
+          'xlink': 'http://www.w3.org/1999/xlink',
+          'flvc': 'info:flvc/manifest/v1'}
